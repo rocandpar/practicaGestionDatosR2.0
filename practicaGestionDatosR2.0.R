36d97e1e35576f88ecb050e5c2e5ab09e4d9c310
rocandpar <randujarpareja@gmail.com>
2024-02-12 12:49:28 +0100

    Update practicaGestionDatosR2.0.R

--- practicaGestionDatosR2.0.R
+++ practicaGestionDatosR2.0.R
@@ -20,7 +20,8 @@ print(materiales_encontrados)
 
 #5
 años_excavaciones <- c(2001,2002,2003,2004,2005)
-
+length(años_excavaciones)
+print(años_excavaciones)
 
 #6
 matriz6 <- matrix(c(1,2,3,4,5), 
@@ -29,15 +30,18 @@ matriz6 <- matrix(c(1,2,3,4,5),
 rownames(matriz6) <- c(2009,1987,2023,2001)
 colnames(matriz6) <- c("munigua","italica","baelo","carteia")
 rowSums(matriz6)
+which.max(rowSums(matriz6))
 print(matriz6)
 
 #7
-matriz7 <- matrix(c("IVaC","VIaC","VIaC","IIIdC"),
+matriz7 <- matrix(c(4,6,6,3,5),
                   nrow = 4, ncol = 3,
                   byrow = F)
 rownames(matriz7) <- c("Dama de Baza", "Tesoro del Carambolo", "Tesorillo de la Algaida", "Tesoro de Tomares")
 colnames(matriz7) <- c("Sevilla", "Cádiz", "Granada")
 print(matriz7)
+colMeans(matriz7)
+which.min(colMeans(matriz7))
 
 #8
 matriz8 <- matrix(c(100,200,300,450,600),
@@ -46,6 +50,7 @@ matriz8 <- matrix(c(100,200,300,450,600),
 rownames(matriz8) <- c("Neolítico Antiguo", "Neolítico Medio", "Neolítico Final")
 colnames(matriz8) <- c("ceramica", "hoz", "raspador")
 rowSums(matriz8)
+which.max(rowSums(matriz8))
 print(matriz8)
 
 #9
@@ -72,6 +77,7 @@ registro_artefactos <- data.frame(
   tipo_artefactos = c("ceramica","adorno", "ceramica"),
   fecha_descubrimiento = c(2023,2024,2020),
   descripcion = c("dressel20","anillo","pellicer1"))
+print(registro_artefactos)
 
 #12
 excavaciones_equipo <- data.frame(
@@ -80,6 +86,7 @@ excavaciones_equipo <- data.frame(
   inicio = c("mayo","septiembre","junio"),
   fin = c("junio","noviembre","agosto")
 )
+print(excavaciones_equipo)
 
 #13
 datos_esqueletos <- data.frame(
@@ -88,6 +95,7 @@ datos_esqueletos <- data.frame(
   sexo = c("fem","mas","fem"),
   caracteristicas = c("decubito supino","trauma","embarazada")
 )
+print(datos_esqueletos)
 
 #14
 ubicaciones_geograficas <- data.frame(
@@ -96,3 +104,4 @@ ubicaciones_geograficas <- data.frame(
   longitud = c(100,200,300),
   altitud = c(2,200,34)
 )
+print(ubicaciones_geograficas)
